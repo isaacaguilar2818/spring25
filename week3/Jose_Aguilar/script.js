49364907446fd69d8c65e278c9f0a9666b4811d7
isaac <isaacaguilar2818@berkeley.edu>
2025-02-20 20:18:53 -0800

    This is the 3rd week. I will continue at home.

---
+++ week3/Jose_Aguilar/script.js
@@ -0,0 +1,6 @@
+function addOneToCounter(){
+    document.getElementById("counter").innerText = 
+    parseInt(document.getElementById("counter").innerText) + 1;
+}
+
+console.log("Hello! I'm Jose Aguilar. This does not need Execution.");
