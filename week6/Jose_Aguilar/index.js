129b0181d32ca2ae15c1169d5e8e0f37e0944433
Isaac Aguilar <isaacaguilar2818@berkeley.edu>
2025-04-03 19:55:43 -0700

    This is the last copy of Week 7.

--- week6/Jose_Aguilar/index.js
+++ week6/Jose_Aguilar/index.js
@@ -4,15 +4,31 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+const mongoose = require("mongoose");
 
-app.get('/', (req, res) => {
+const Schema = mongoose.Schema;
+
+const messageSchema = new Schema({
+  content: { type: String }
+})
+
+const messageModel = mongoose.model("Message", messageSchema)
+
+app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    const message = new messageModel();
+    message.content = msg;
+    message.save().then(m => {
+      io.emit('chat message', msg);
+    })
+  });
 });
 
-server.listen(3000, () => {
+server.listen(3000, async function(){
+  await mongoose.connect("mongodb+srv://sam:helloworld@cluster0.ytnrk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
   console.log('listening on *:3000');
 });
